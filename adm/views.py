10e0ee8fcc03a3a7564cdea29ecd8233eab302c2
Gabriell <gabriellsamy08@gmail.com>
2024-10-16 12:00:16 -0300

    fazendo links teste

--- adm/views.py
+++ adm/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Olá mundo');
+def index(request):
+    return render(request, "adm/index.html");
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from adm.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), path ("", index),
 ]
