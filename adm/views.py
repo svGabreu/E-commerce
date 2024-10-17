af3380861269a4381826686a43ea73cec1ae42b8
Gabriell <gabriellsamy08@gmail.com>
2024-10-17 11:54:42 -0300

    teste banco de dados/links views e urls

--- adm/views.py
+++ adm/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from .models import Adm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+class admListView(ListView):
+    model = Adm
+
+class admCreateView(CreateView):
+    model = Adm
+    fields=["title","deadline"]
+    success_url = reverse_lazy("")
+    
+class admUpdateView(UpdateView):
+    model = Adm
+
+class admDeleteView(DeleteView):
+    model = Adm
+
 
 def index(request):
-    return render(request, "adm/index.html");
+    return render(request, "adm/index.html")
--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from adm.views import index
+#from adm.views import index
+from adm.views import admListView
+from adm.views import admCreateView
+from adm.views import admUpdateView
+from adm.views import admDeleteView
+from django.urls import reverse_lazy
 
 urlpatterns = [
-    path('admin/', admin.site.urls), path ("", index),
+    path("admin/", admin.site.urls),
+    path("", admListView.as_view()),
+    path("create", admCreateView.as_view()),
+    path("update", admUpdateView.as_view()),
+    path("delete", admDeleteView.as_view())
 ]
