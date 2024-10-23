80755c4c37a9eda834c378db36acd483948d2744
Gabriell <you@example.com>
2024-10-22 21:28:45 -0300

    att em sala

--- setup/urls.py
+++ setup/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 #from adm.views import index
-from adm.views import admListView
-from adm.views import admCreateView
-from adm.views import admUpdateView
-from adm.views import admDeleteView
+from adm.views import admListView, admCreateView, admUpdateView, admDeleteView
 from django.urls import reverse_lazy
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", admListView.as_view()),
-    path("create", admCreateView.as_view()),
-    path("update", admUpdateView.as_view()),
-    path("delete", admDeleteView.as_view())
+    path("", admListView.as_view(),name='adm_list'),
+    path("create", admCreateView.as_view(),name='adm_create'),
+    path("update/<int:pk>/", admUpdateView.as_view(),name='adm_update'),
+    path("delete/<int:pk>/", admDeleteView.as_view(),name='adm_delete')
 ]
